5510429fb4261c309f2400d3561ad0d572de1b87
Alex Conrad <amconrad96@icloud.com>
2022-07-28 10:56:20 -0500

    latest

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
 import sign from "../../assets/karsten-winegeart-0Wra5YYVQJE-unsplash.jpeg";
-import create from "../../assets/Workout app image 1.jpeg";
+import create from "../../assets/Workout-app-image-1.jpeg";
 import find from "../../assets/humphrey-muleba-LOA2mTj1vhc-unsplash.jpg";
 
 export default () => html`
