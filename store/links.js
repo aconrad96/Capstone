0d3208a3139e8fdfd84d3eb47e10285aae3402d3
Alex Conrad <amconrad96@icloud.com>
2022-07-28 21:46:32 -0500

    deleted links.js

--- store/links.js
+++
@@ -1,6 +0,0 @@
-export default [
-  { title: "Home", text: "Home" },
-  { title: "Create", text: "Create a Plan" },
-  { title: "Find", text: "Find a Gym" },
-  { title: "Contact", text: "Contact" }
-];
